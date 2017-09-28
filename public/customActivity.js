d907f2b2d156316897cbec28fbfe684fa0b45b9d
jfraileg <jfraileg@everis.com>
2017-09-28 17:52:22 +0200

    Update customActivity.js

--- public/customActivity.js
+++ public/customActivity.js
@@ -59,7 +59,7 @@ define(function (require) {
 		payload['arguments'] = payload['arguments'] || {};
 		payload['arguments'].execute = payload['arguments'].execute || {};
 		payload['arguments'].execute.inArguments = [{
-			'serviceCloudId': '{{Event.' + eventDefinitionKey + '.\"<EVENT DATA ID PATH>\"}}'
+			'serviceCloudId': '{{Event.' + eventDefinitionKey + '.\"VehicleRelationship__c_Salesforce:VR_Contact__c\"}}'
 		}];
 
 		payload['metaData'] = payload['metaData'] || {};
